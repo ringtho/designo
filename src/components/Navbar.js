ed610dad530db1b0504718ff8a2be154b7b83911
Smith Ringtho <sringtho@gmail.com>
2023-04-10 21:47:25 +0300

    Added state for toggling the hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,38 @@
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import iconClose from "../assets/shared/mobile/icon-close.svg"
+import iconHamburger from "../assets/shared/mobile/icon-hamburger.svg"
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
+    console.log(isMenuOpen)
+
     return (
         <header className="nav-items">
             <div className="logo-container">
                 <div className="header-circle"></div>
-                <h1 className="title">DESIGNO</h1>
+                <Link to="/" className="designo-title-link">
+                    <h1 className="title">DESIGNO</h1>
+                </Link>
             </div>
             <div className="icon-container">
-            <i className="fa-solid fa-bars"></i>
+                { isMenuOpen ?
+                <img 
+                    src={iconClose} 
+                    alt="icon-close" 
+                    className="navbar-icon" 
+                    onClick={toggleMenu}
+                /> : 
+                <img 
+                    src={iconHamburger} 
+                    alt="icon-hamburger" 
+                    className="navbar-icon" 
+                    onClick={toggleMenu} 
+                />
+                }   
             </div>
         </header>
     )
