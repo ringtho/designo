0b2b5de5a3a78675e7e7d692c2f70b235902a4d9
Smith Ringtho <sringtho@gmail.com>
2023-04-11 23:26:01 +0300

    Fixed the positioning of the get in touch element

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,13 +16,13 @@ const Home = () => {
             
             <section className="hero">
                 <div className="hero-container">
-                <div className="hero-content">
-                    <h2>Award-winning custom designs and digital branding solutions</h2>
-                    <p>With over 10 years in the industry, we are experienced in creating 
-                        fully responsive websites, app design, and engaging brand experiences. 
-                        Find out more about our services.</p>
-                    <button className="hero-btn">LEARN MORE</button>
-                </div>
+                    <div className="hero-content">
+                        <h2>Award-winning custom designs and digital branding solutions</h2>
+                        <p>With over 10 years in the industry, we are experienced in creating 
+                            fully responsive websites, app design, and engaging brand experiences. 
+                            Find out more about our services.</p>
+                        <button className="hero-btn">LEARN MORE</button>
+                    </div>
                 <div className="hero-image"></div>
                 </div>
             </section>
