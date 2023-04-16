fb9d391165c8b609113523f9d25837dd187524ce
Smith Ringtho <sringtho@gmail.com>
2023-04-16 08:26:19 +0300

    Fixed the contact page positioning

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,24 +10,26 @@ const Contact = () => {
             <main>
                 <section className="form-container">
                 
-                    <div className="contact-wrapper">
-                        <div className="contact-heading">
-                            <h1>Contact Us</h1>
-                            <p>
-                                Ready to take it to the next level? Let’s talk about 
-                                your project or idea and find out how we can help 
-                                your business grow. If you are looking for unique 
-                                digital experiences that’s relatable to your users, 
-                                drop us a line.
-                            </p>
+                    <div className="contact-container">
+                        <div className="contact-wrapper">
+                            <div className="contact-heading">
+                                <h1>Contact Us</h1>
+                                <p>
+                                    Ready to take it to the next level? Let’s talk about 
+                                    your project or idea and find out how we can help 
+                                    your business grow. If you are looking for unique 
+                                    digital experiences that’s relatable to your users, 
+                                    drop us a line.
+                                </p>
+                            </div>
+                            <form className="form" action="#">
+                                <input type="name" name="name" required placeholder="Name" />
+                                <input type="email" name="email" required placeholder="Email" />
+                                <input type="text" name="phone" placeholder="Phone" />
+                                <textarea name="message" placeholder="Your Message" />
+                                <button type="submit">SUBMIT</button>
+                            </form>
                         </div>
-                        <form className="form" action="#">
-                            <input type="name" name="name" required placeholder="Name" />
-                            <input type="email" name="email" required placeholder="Email" />
-                            <input type="text" name="phone" placeholder="Phone" />
-                            <textarea name="message" placeholder="Your Message" />
-                            <button type="submit">SUBMIT</button>
-                        </form>
                     </div>
 
                 </section>
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -14,17 +14,19 @@ const Location = () => {
             <section className="about-hero">
                 <img src={canadaMap} alt="canada" className="about-hero-image" />
                 <div className="about-descriptions">
-                    <div className="about-description-content">
-                        <h2>Canada</h2>
-                        <div className="location-container">
-                            <h4>Designo Central Office</h4>
-                            <p>3886 Wellington Street</p>
-                            <p>Toronto, Ontario M9C 3J5</p>
-                        </div>
-                        <div className="location-header">
-                            <h4>Contact</h4>
-                            <p>P : +1 253-863-8967</p>
-                            <p>M : contact@designo.co</p>
+                    <div className="about-descriptions-wrapper">
+                        <div className="about-description-content">
+                            <h2>Canada</h2>
+                            <div className="location-container">
+                                <h4>Designo Central Office</h4>
+                                <p>3886 Wellington Street</p>
+                                <p>Toronto, Ontario M9C 3J5</p>
+                            </div>
+                            <div className="location-header">
+                                <h4>Contact</h4>
+                                <p>P : +1 253-863-8967</p>
+                                <p>M : contact@designo.co</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -32,17 +34,19 @@ const Location = () => {
             <section className="about-hero">
                 <img src={australiaMap} alt="australia" className="about-hero-image" />
                 <div className="about-descriptions">
-                    <div className="about-description-content">
-                        <h2>Australia</h2>
-                        <div className="location-container">
-                            <h4>Designo AU Office</h4>
-                            <p>19 Balonne Street</p>
-                            <p>New South Wales 2443</p>
-                        </div>
-                        <div className="location-header">
-                            <h4>Contact</h4>
-                            <p>P : (02) 6720 9092</p>
-                            <p>M : contact@designo.au</p>
+                    <div className="about-descriptions-wrapper">
+                        <div className="about-description-content">
+                            <h2>Australia</h2>
+                            <div className="location-container">
+                                <h4>Designo AU Office</h4>
+                                <p>19 Balonne Street</p>
+                                <p>New South Wales 2443</p>
+                            </div>
+                            <div className="location-header">
+                                <h4>Contact</h4>
+                                <p>P : (02) 6720 9092</p>
+                                <p>M : contact@designo.au</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,17 +54,19 @@ const Location = () => {
             <section className="about-hero" id="uk">
                 <img src={ukMap} alt="united-kingdom" className="about-hero-image" />
                 <div className="about-descriptions">
-                    <div className="about-description-content">
-                        <h2>United Kingdom</h2>
-                        <div className="location-container">
-                            <h4>Designo UK Office</h4>
-                            <p>13  Colorado Way</p>
-                            <p>Rhyd-y-fro SA8 9GA</p>
-                        </div>
-                        <div className="location-header">
-                            <h4>Contact</h4>
-                            <p>P : 078 3115 1400</p>
-                            <p>M : contact@designo.uk</p>
+                    <div className="about-descriptions-wrapper">
+                        <div className="about-description-content">
+                            <h2>United Kingdom</h2>
+                            <div className="location-container">
+                                <h4>Designo UK Office</h4>
+                                <p>13  Colorado Way</p>
+                                <p>Rhyd-y-fro SA8 9GA</p>
+                            </div>
+                            <div className="location-header">
+                                <h4>Contact</h4>
+                                <p>P : 078 3115 1400</p>
+                                <p>M : contact@designo.uk</p>
+                            </div>
                         </div>
                     </div>
                 </div>
