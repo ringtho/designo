d90f065e45c469a65529dbd6e723e61ca87eb81c
Smith Ringtho <sringtho@gmail.com>
2023-04-18 12:14:01 +0000

    Fixed the navbar, footer and get in touch for desktop mode

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -13,11 +13,13 @@ const GetInTouch = () => {
             <div className="get-in-touch">
                 <div className="get-in-touch-wrapper">
                     <div className="get-in-touch-container">
-                        <h2>Let’s talk about your project</h2>
-                        <p>
-                        Ready to take it to the next level? Contact us today and find out 
-                        how our expertise can help your business grow.
-                        </p>
+                        <div className="get-in-touch-desc">
+                            <h2>Let’s talk about your project</h2>
+                            <p>
+                            Ready to take it to the next level? Contact us today and find out 
+                            how our expertise can help your business grow.
+                            </p>
+                        </div>
                         <button className="footer-btn" onClick={onClick}>GET IN TOUCH</button>
                     </div>
                 </div>
